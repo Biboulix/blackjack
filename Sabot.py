103c792ebd5771dfb62574a7cc78c401bbeb193a
Biboulix <38279595+Biboulix@users.noreply.github.com>
2019-04-10 10:16:59 +0200

    Add files via upload

---
+++ Sabot.py
@@ -0,0 +1,42 @@
+#coding : utf-8
+import random
+#---------------------------------------------------------------------#
+#joueur
+def joueur() :
+	
+
+#---------------------------------------------------------------------# 
+#pioche
+
+def sabot() :
+	valeur == 0 
+	sabot = random.randint(1, 13)
+	if sabot == 11 : 
+		print("valet")
+		valeur == 10
+		return valeur
+
+	elif sabot == 12 : 
+		print("dame")
+		valeur == 10
+		return valeur
+	elif sabot == 13 :
+		print("roi")
+		valeur == 10
+		return valeur
+	else :
+		print(sabot)
+		valeur == sabot
+		return valeur
+#--------------------------------------------------------------------#
+#tour de distribution
+for i in range (2) : 
+	pioche = sabot()
+	print(pioche)
+#--------------------------------------------------------------------#
+#tour_n
+
+
+ 
+
+
