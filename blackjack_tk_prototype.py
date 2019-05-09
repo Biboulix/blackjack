f09d5ec217ddd862829282f467eb5dc500f4dbdc
Arian Dervishaj <arian@arians-macbook-pro.home>
2019-05-09 21:47:27 +0200

    image

--- blackjack_tk_prototype.py
+++ blackjack_tk_prototype.py
@@ -1,6 +1,74 @@
 from tkinter import *
-from random import *
 from bibli import *
+import random
+
+#---------------------------------------------------------------------------#
+#Fonctions
+def image(sabot,nb_carte):
+    if nb_carte <=6:
+        coord_y = coord_y_joueur[0]
+    else:
+        coord_y = coord_y_joueur[1]
+
+    carte = sabot * 4 - 1
+    couleur = random.randint(0,3)
+    carte_finale = carte - couleur
+    img = PhotoImage(file=liste_carte[carte_finale])
+    can.image= img
+    can.create_image(coord_x[nb_carte-1],coord_y, image=img, anchor = NW)
+
+#valeur de l'as
+def valeur_as() :
+
+	vrai_valeur_as = int(input ("voulez vous que l'as valent 1 ou 11 ?"))
+
+	if vrai_valeur_as == 1 :
+		print("Votre as vaut désormais 1")
+		return vrai_valeur_as
+
+	else :
+		print("votre as vaut désormais 11")
+		return 11
+
+# Tirage des cartes
+def sabot() :
+    global nb_carte
+    valeur = 0
+    print("Total ", valeur)
+    sabot = random.randint(0,13)
+    nb_carte = nb_carte + 1
+
+    if sabot == 11 :
+        print("valet")
+        valeur = valeur + 10
+        image(sabot,nb_carte)
+
+    elif sabot == 12 :
+        print("dame")
+        valeur = valeur + 10
+        image(sabot,nb_carte)
+
+    elif sabot == 13 :
+        print("roi")
+        valeur = valeur + 10
+        image(sabot,nb_carte)
+
+    elif sabot == 1 :                     #appelle la fonction as
+        as_valeur = valeur_as()
+        if as_valeur == 1 :
+            valeur = valeur + 1
+            image(sabot,nb_carte)
+
+        else:
+            valeur = valeur + 11
+            image(sabot,nb_carte)
+    else :
+        print(sabot)
+        valeur = valeur + sabot
+        image(sabot,nb_carte)
+
+    print("Vous avez un total de ", valeur)
+
 
 #------------------------------------------------------------#
 # Fenetre master
@@ -13,8 +81,8 @@ fen.title('BlackJack')
 
 can = Canvas(fen, width =1030, height =850, bg ='sea green')
 
-img = PhotoImage(file="logo.gif")
-can.create_image(515,425, image=img)
+logo = PhotoImage(file="logo.gif")
+can.create_image(515,425, image=logo)
 
 #--------------------------------------------------------------#
 # Case blanches haut #
@@ -41,17 +109,15 @@ for f in range(0,6):
 
 #--------------------------------------------------------------#
 # Test de posage de cartes
-k = randint(0,12)
-coul = randint(0,3)
-car = k * 4 + coul
-carte = PhotoImage(file=liste_carte[car])
-can.create_image(coord_x[0],coord_y_joueur[0], image=carte, anchor = NW)
+
+nb_carte = 0
 
 
 #--------------------------------------------------------------#
 # Bouton #
 
 Button(fen,text='Quitter',command=fen.quit, highlightbackground='#3E4149').grid(row=0,column=1)
+Button(fen,text='Carte',command=sabot, highlightbackground='#3E4149').grid(row=0,column=2)
 
 #--------------------------------------------------------------#
 # Initatilisation de fenetre master
